ce76457f6e28c438da42b7bff0dfd3a75c67d001
gabrielcroitoru97 <gabrielcroitoru@hotmail.com>
2024-07-16 02:13:08 +0000

    Creates movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,21 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def post
+
+
+    @new_movie=Movie.new
+    @new_movie.title=params.fetch("the_title")
+    @new_movie.year=params.fetch("the_year")
+    @new_movie.duration=params.fetch("the_duration")
+    @new_movie.description=params.fetch("the_description")
+    @new_movie.image=params.fetch("the_image")
+    @new_movie.director_id=params.fetch("the_director_id")
+    @new_movie.save
+
+    redirect_to("/movies")
+ 
+  end
+
 end
